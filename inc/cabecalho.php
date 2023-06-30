95ec32b5597c12a8cf4a0b5d0441770ecc067169
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-06-30 20:29:28 -0300

    .

---
+++ inc/cabecalho.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema</title>
+    <!-- bootstrap css -->
+    <!-- bootstrap icons -->
+    <!-- meu estilo.css -->
+</head>
+
+<body>
+    
+<header>
+    <center>
+        <a href="../painel/index.php">
+        <img src="../img/pranta.png" widht="10px" height="100px">
+        <br>
+        </a>
+    </center>
+</header>
---
+++ inc/rodape.php
@@ -0,0 +1,10 @@
+<footer >
+    <p style="text-align: center;">
+    &copy; Todos os direitos reservados. Criado por 🤍 <a href="mailto::diegorodriguesdantas02@gmail.com" >Diego Dantas</a>
+    </p>
+</footer>
+<!-- bootstrap js  -->
+<!-- meu js -->
+
+</body>
+</html>
